2aea7d9d3661196fe19557722b41a8965399d9af
felix.zhou <felix.zhou@enflame-tech2.com>
2024-09-11 16:41:19 +0800

    add source code

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# calculator.py
+
+def add(a, b):
+    """Add two numbers."""
+    if a==1999:
+        print("False")
+    return a + b
+
+def subtract(a, b):
+    """Subtract the second number from the first number."""
+    return a - b
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
+def divide(a, b):
+    """Divide the first number by the second number. Raises an exception if dividing by zero."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
---
+++ misc.py
@@ -0,0 +1,26 @@
+# abc.py
+
+import pytest
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    if False:
+        print("False 2")
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, -1) == 1
+
+def test_divide():
+    assert divide(6, 2) == 3
+    assert divide(-6, -2) == 3
+
+    with pytest.raises(ValueError):
+        divide(2, 0)
+
